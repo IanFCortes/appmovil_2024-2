6ea5b14b24554945134666f599eecdd6453d9b77
El profe <al.sepulvedam@profesor.duoc.cl>
2024-08-28 08:57:01 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'elementos_ionic_s3',
+  webDir: 'www'
+};
+
+export default config;
